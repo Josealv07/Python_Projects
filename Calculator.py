6171acf5b48ecd329e9836e7a446692d7ab396bc
Jose AlVarez <Jose07alvarezm@gmail.com>
2023-04-24 16:04:17 -0500

    feat: cambiando colores / changing colors

--- Calculator.py
+++ Calculator.py
@@ -1,37 +1,39 @@
-from tkinter import Tk, Entry, Button, StringVar
+from tkinter import * #Tk, Entry, Button, StringVar, PhotoImage
 
 
 class Calculator:
     def __init__(self,main):
         main.title("Calculadora by Jose Alvarez")
         main.geometry('357x420+0+0')
-        main.config(bg = 'grey')
+        main.config(bg = 'black')
+        p1 = PhotoImage(file = 'Images\Mas-sencillo.png')
+        main.iconphoto(False, p1)
         main.resizable(False,False)
         self.equation = StringVar()
         self.entry_value = ''
         Entry(width = 17, bg='#fff', font=('Arial Bold', 28), textvariable=self.equation).place(x=0, y=0)
 
         #Creación Botones de la Calculadora
-        Button(width=11,height=4, text='(',relief='flat', bg='White', command=lambda:self.show('(')).place(x=0,y=50)
-        Button(width=11,height=4, text=')',relief='flat', bg='White', command=lambda:self.show(')')).place(x=90,y=50)
-        Button(width=11,height=4, text='%',relief='flat', bg='White', command=lambda:self.show('%')).place(x=180,y=50)
-        Button(width=11,height=4, text='1',relief='flat', bg='White', command=lambda:self.show(1)).place(x=0,y=125)
-        Button(width=11,height=4, text='2',relief='flat', bg='White', command=lambda:self.show(2)).place(x=90,y=125)
-        Button(width=11,height=4, text='3',relief='flat', bg='White', command=lambda:self.show(3)).place(x=180,y=125)
-        Button(width=11,height=4, text='4',relief='flat', bg='White', command=lambda:self.show(4)).place(x=0,y=200)
-        Button(width=11,height=4, text='5',relief='flat', bg='White', command=lambda:self.show(5)).place(x=90,y=200)
-        Button(width=11,height=4, text='6',relief='flat', bg='White', command=lambda:self.show(6)).place(x=180,y=200)
-        Button(width=11,height=4, text='7',relief='flat', bg='White', command=lambda:self.show(7)).place(x=0,y=275)
-        Button(width=11,height=4, text='8',relief='flat', bg='White', command=lambda:self.show(8)).place(x=180,y=275)
-        Button(width=11,height=4, text='9',relief='flat', bg='White', command=lambda:self.show(9)).place(x=90,y=275)
-        Button(width=11,height=4, text='0',relief='flat', bg='White', command=lambda:self.show(0)).place(x=90,y=350)
-        Button(width=11,height=4, text='.',relief='flat', bg='White', command=lambda:self.show('.')).place(x=180,y=350)
-        Button(width=11,height=4, text='+',relief='flat', bg='White', command=lambda:self.show('+')).place(x=270,y=275)
-        Button(width=11,height=4, text='-',relief='flat', bg='White', command=lambda:self.show('-')).place(x=270,y=200)
-        Button(width=11,height=4, text='x',relief='flat', bg='White', command=lambda:self.show('*')).place(x=270,y=50)
-        Button(width=11,height=4, text='/',relief='flat', bg='White', command=lambda:self.show('/')).place(x=270,y=125)
-        Button(width=11,height=4, text='=',relief='flat', bg='White', command=self.solve).place(x=270,y=350)
-        Button(width=11,height=4, text='C',relief='flat', bg='White', command=self.clear).place(x=0,y=350)
+        Button(width=11,height=4, text='(',relief='flat', bg='gainsboro', command=lambda:self.show('(')).place(x=0,y=50)
+        Button(width=11,height=4, text=')',relief='flat', bg='gainsboro', command=lambda:self.show(')')).place(x=90,y=50)
+        Button(width=11,height=4, text='%',relief='flat', bg='gainsboro', command=lambda:self.show('%')).place(x=180,y=50)
+        Button(width=11,height=4, text='1',relief='flat', bg='gainsboro', command=lambda:self.show(1)).place(x=0,y=125)
+        Button(width=11,height=4, text='2',relief='flat', bg='gainsboro', command=lambda:self.show(2)).place(x=90,y=125)
+        Button(width=11,height=4, text='3',relief='flat', bg='gainsboro', command=lambda:self.show(3)).place(x=180,y=125)
+        Button(width=11,height=4, text='4',relief='flat', bg='gainsboro', command=lambda:self.show(4)).place(x=0,y=200)
+        Button(width=11,height=4, text='5',relief='flat', bg='gainsboro', command=lambda:self.show(5)).place(x=90,y=200)
+        Button(width=11,height=4, text='6',relief='flat', bg='gainsboro', command=lambda:self.show(6)).place(x=180,y=200)
+        Button(width=11,height=4, text='7',relief='flat', bg='gainsboro', command=lambda:self.show(7)).place(x=0,y=275)
+        Button(width=11,height=4, text='8',relief='flat', bg='gainsboro', command=lambda:self.show(8)).place(x=180,y=275)
+        Button(width=11,height=4, text='9',relief='flat', bg='gainsboro', command=lambda:self.show(9)).place(x=90,y=275)
+        Button(width=11,height=4, text='0',relief='flat', bg='gainsboro', command=lambda:self.show(0)).place(x=90,y=350)
+        Button(width=11,height=4, text='.',relief='flat', bg='azure', command=lambda:self.show('.')).place(x=180,y=350)
+        Button(width=11,height=4, text='+',relief='flat', bg='azure', command=lambda:self.show('+')).place(x=270,y=275)
+        Button(width=11,height=4, text='-',relief='flat', bg='azure', command=lambda:self.show('-')).place(x=270,y=200)
+        Button(width=11,height=4, text='x',relief='flat', bg='azure', command=lambda:self.show('*')).place(x=270,y=50)
+        Button(width=11,height=4, text='/',relief='flat', bg='azure', command=lambda:self.show('/')).place(x=270,y=125)
+        Button(width=11,height=4, text='=',relief='flat', bg='lawn green', command=self.solve).place(x=270,y=350)
+        Button(width=11,height=4, text='C',relief='flat', bg='lawn green', command=self.clear).place(x=0,y=350)
 
     #metodo para definir que se va mostrando en la pantalla de la calculadora
     def show(self, value):
